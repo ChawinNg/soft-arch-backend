f67a587df3fd80d64501e850d8151897583103a9
ChawinNg <ik.chawin@gmail.com>
2024-11-09 16:28:40 +0700

    chores: remove unused

--- backend/cmd/courses/main.go
+++ backend/cmd/courses/main.go
@@ -39,17 +39,13 @@ func main() {
 	apiv1.Put("/sections/:id", sectionHandler.UpdateSection)
 	apiv1.Delete("/sections/:id", sectionHandler.DeleteSection)
 
-	apiv1.Get("/courses", courseHandler.GetCourses)
 	apiv1.Get("/courses/search", courseHandler.IndexCourses)
 	apiv1.Get("/courses/paginated", courseHandler.GetCoursesPaginated)
 	apiv1.Get("/courses/:id", courseHandler.GetCourse)
+	apiv1.Get("/courses", courseHandler.GetCourses)
 	apiv1.Post("/courses", courseHandler.CreateCourse)
 	apiv1.Put("/courses/:id", courseHandler.UpdateCourse)
 	apiv1.Delete("/courses/:id", courseHandler.DeleteCourse)
 
 	log.Fatal(app2.Listen(os.Getenv("BACKEND_REST")))
-}
-
-func helloHandler(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"message": "Hello"})
 }
